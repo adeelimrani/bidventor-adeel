3610067f0d27005bc66f93aaf4561ebe8904bd41
HamasAtr <hamasakhtar5@gmail.com>
2025-02-15 17:24:49 +0500

    add deployment tweeks

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
+const isProtectedRoute = createRouteMatcher(['/admin(.*)'])
 export default clerkMiddleware();
 
 export const config = {
